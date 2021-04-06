c8532c07afb8f57852cdcb0be1b9819bb8e5a54e
Chris Fried <fried.chris@gmail.com>
2021-04-05 21:20:32 -0500

    Just going to put 'em right in the source, I guess.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,10 @@
-import { bungieProd } from './keys';
-
 export const environment = {
   production: true,
-  bungie: bungieProd
-};
+  bungie: {
+    apiKey: '25cec645b08c40a393deccc37749b2c6',
+    authUrl: 'https://www.bungie.net/en/OAuth/Authorize',
+    clientId: '30141',
+    clientSecret: 'Qh0txds-DSzcPNS6kl6RVBFqtMxVkQK7Cs40wbB3C78',
+    redirect: 'https://chrisfried.github.io/secret-scrublandeux',
+  },
+}
