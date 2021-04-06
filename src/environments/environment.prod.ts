5dde86aa42967473795942bc7dca5f9d0e56f0d3
Chris Fried <fried.chris@gmail.com>
2021-04-05 21:52:55 -0500

    wut

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   bungie: {
-    apiKey: '25cec645b08c40a393deccc37749b2c6',
+    apiKey: 'd42e07f74a8c469faace94a56dbdbfbc',
     authUrl: 'https://www.bungie.net/en/OAuth/Authorize',
-    clientId: '30141',
-    clientSecret: 'Qh0txds-DSzcPNS6kl6RVBFqtMxVkQK7Cs40wbB3C78',
+    clientId: '30710',
+    clientSecret: 'E7GetUCU3jUXiXRC2MDaGUbLVUv9ddS1dDW.DnXT3k0',
     redirect: 'https://chrisfried.github.io/secret-scrublandeux',
   },
 }
