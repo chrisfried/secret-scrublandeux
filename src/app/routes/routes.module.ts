489bc0580ddfceb23bc4950ee213f028bcb9b60e
Chris Fried <fried.chris@gmail.com>
2023-03-06 20:13:03 -0600

    Upgrading...

--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -8,7 +8,7 @@ const appRoutes: Routes = [
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(appRoutes, {})],
   exports: [RouterModule],
 })
 export class RoutesModule {}
