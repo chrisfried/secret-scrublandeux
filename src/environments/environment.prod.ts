5d0a4fd133652d693959e1766d43c2d5aad0e4d7
Chris Fried <fried.chris@gmail.com>
2024-03-09 14:45:30 -0600

    Season time per week/day

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
     apiKey: 'cd01c0cb258c48228cdbc0f468674257',
     authUrl: 'https://www.bungie.net/en/OAuth/Authorize',
     clientId: '36111',
+    clientSecret: 'YAhIT2LE3yUyFoWqyGMkJe75BTeJuyC1IawKlb9G4Ao',
     redirect: 'https://d2scrub.land/auth',
   },
 }
